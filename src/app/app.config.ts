e8f8faaa5bd98ce1cf42610bbcb0c1065926521c
wmdaini <wajdimd@gmail.com>
2025-09-07 01:29:36 +0100

    integrate and configure translation using ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideHttpClient(),
+    provideTranslateService({
+      lang: 'en',        // initial language (optional here)
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      })
+    }),
+
+    provideRouter(routes),
+    provideClientHydration(withEventReplay())
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe, TranslateDirective, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TranslatePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('angular-demo');
+  translationLanguagesList: string[] = ['en','fr','nl','ge'];
+  constructor(private translate: TranslateService) {
+    // default + auto-detect browser language (basic example)
+    this.translate.setDefaultLang('en');
+    const browserLang:string = navigator.language?.split('-')[0] ?? 'en';
+    this.translate.use(this.translationLanguagesList.includes(browserLang) ? browserLang : 'en');
+  }
+
+  switchLang(lang:string) {
+    this.translate.use(lang);
+  }
 }
